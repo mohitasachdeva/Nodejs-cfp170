829763d3246892a174583727dcef35512ab2727e
HP <sachdevamohita5@gmail.com>
2022-09-09 22:31:31 +0530

    parameterize function in js

--- Function call.js
+++ Function call.js
@@ -1,8 +1,15 @@
-function fname(){
-var a =10;
-var b = 20;
-let c = a+b;
+//non parameterize function 
+// function fname(){
+// var a =10;
+// var b = 20;
+// let c = a+b;
 
-console.log(c)
-}
-fname();
+// console.log(c)
+// }
+// fname();
+//parameterize function
+function product(a , b ){
+    let c = a*b;
+    console.log( c )
+} 
+product(10 , 20 );
